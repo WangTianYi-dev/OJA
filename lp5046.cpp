39ddb4248f6d60948ad3b9e02f44022b1e7d0a40
WangTianYi-dev <daerhoy@icloud.com>
2021-03-13 09:45:53 +0800

    '

--- lp5046.cpp
+++ lp5046.cpp
@@ -2,5 +2,12 @@
 #include <iostream>
 const int maxn = 1e9 + 10;
 struct Node {
-    int value, left, right, count, size;
-} tree[
+    int value, left, right, count, subsize;
+} tree[maxn];
+
+void add(int sub, int val) {
+    tree[sub].subsize++;
+    if (tree[sub].value == val) {
+
+    }
+};
