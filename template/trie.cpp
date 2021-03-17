237b3b3f7ff5fd77d94ada28c9a428c0b86067d0
WangTianYi-dev <daerhoy@icloud.com>
2021-03-17 19:13:45 +0800

    1

---
+++ template/trie.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Trie {
+public:
+    Trie* next[26] = { nullptr };
+    bool end = false;
+
+    /** Initialize your data structure here. */
+    Trie()
+    {
+    }
+
+    /** Inserts a word into the trie. */
+    void insert(string word)
+    {
+        Trie* p = this;
+        for (const auto& i : word) {
+            if (p->next[i - 'a']) {
+                p = p->next[i - 'a'];
+            } else {
+                p = p->next[i - 'a'] = new Trie();
+            }
+        }
+        p->end = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word)
+    {
+        Trie* p = this;
+        for (const auto& i : word) {
+            if (p->next[i - 'a']) {
+                p = p->next[i - 'a'];
+            } else {
+                return false;
+            }
+        }
+        return p->end;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix)
+    {
+        Trie* p = this;
+        for (const auto& i : prefix) {
+            if (p->next[i - 'a']) {
+                p = p->next[i - 'a'];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+
+int main()
+{
+    Trie* o = new Trie();
+    o->insert("apple");
+    cout << o->search("apple");
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+struct s
+{
+    static int i;
+    s() {
+        i++;
+    }
+} a[1000];
+int s::i = 0;
 
 int main()
 {
+    cout << s::i << endl;
+    return 0;
 }
